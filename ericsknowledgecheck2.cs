7af813a1d9e6bf9a24e6b16d03cc60418e120981
ericc <ericcantrell93@gmail.com>
2023-12-24 09:40:56 -0500

    Knowledgecheck2

---
+++ ericsknowledgecheck2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Myrecords
+{
+    public class Wins
+
+    {
+        int Win { get; set; }
+        string? Victory { get; set; }
+    }
+
+    public class Losses : Wins
+    {
+        int Loss { get; set; }
+        string? Defeat { get; set; }
+    }
+
+    internal class recordstats();
+    {
+      private
+    }
+
+    Console.WriteLine("How many records do you want to add? ");
+    var numberOfRecords = int.Parse(Console.ReadLine());
+
+    var recordList = new List<Wins>();
+    for (int i = 0; i<numberOfRecords; i++)
+    
+	var myClass = new MyClass();
+    Console.WriteLine("Enter the value for ");
+	myClass.Wins = Console.ReadLine();
+
+	recordList.Add();
+}
